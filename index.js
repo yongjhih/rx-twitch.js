13dd774e4b5c3ef940f05f864f1dabbe185825a6
Andrew Chen <yongjhih@gmail.com>
2016-06-27 07:52:04 +0800

    Testing

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function RxTwitch() {
 
 RxTwitch.Fetch = fetch;
 function fetch(url, options) {
-  return Rx.Observable.fromPromise(Fetch(url, options)).flatMap(function (response) {
-    return Rx.Observable.concat(Rx.Observable.just(response), (response.json()._links && response.json()._links.next && response.json()._cursor)
+  return Rx.Observable.fromPromise(Fetch(url)).flatMap(function (response) {
+    return Rx.Observable.concat(Rx.Observable.fromPromise(response.json()), (response.json()._links && response.json()._links.next && response.json()._cursor)
         ? fetch(response.json()._links.next) : Rx.Observable.empty());
   });
 }
@@ -21,6 +21,8 @@ function follows(user, baseUrl) {
   if (!baseUrl) baseUrl = 'https://api.twitch.tv';
 
   var url = baseUrl + `/kraken/channels/${user}/follows`;
-  fetch(url).map(function (response) { return response.json(); })
-    .flatMap(function (json) { return Rx.Observable.from(json.follows); });
+  console.log(url);
+  return fetch(url).flatMap(function (json) { return Rx.Observable.from(json.follows); });
 }
+
+/* vim: set sw=2: */
